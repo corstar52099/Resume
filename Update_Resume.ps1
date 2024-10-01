e62e4219eed017864166709fab5c452cabce3eb1
corstar52099 <coryd.starks@gmail.com>
2024-10-01 12:15:03 -0700

    updating script to delete and rename files properly

--- Update_Resume.ps1
+++ Update_Resume.ps1
@@ -6,7 +6,7 @@ param (
 $newFileBaseName = "Cory Starks Resume"
 $newDocxFile = "$newFileBaseName.docx"
 $newPdfFile = "$newFileBaseName.pdf"
-$scriptFile = $MyInvocation.MyCommand.Name  # This gets the name of the script file itself
+$scriptFile = $MyInvocation.MyCommand.Name  # Get the name of the script itself
 
 # Identify the most recently modified .docx and .pdf files in the current directory
 $latestDocx = Get-ChildItem -Filter "*.docx" | Where-Object { $_.Name -ne $scriptFile } | Sort-Object LastWriteTime -Descending | Select-Object -First 1
@@ -18,20 +18,21 @@ if (-not $latestDocx -or -not $latestPdf) {
     exit 1
 }
 
-# Delete old versions of the files from the repository
-$oldFiles = Get-ChildItem -Filter "Cory Starks Resume.*" | Where-Object { $_.Name -ne $scriptFile }
-foreach ($file in $oldFiles) {
-    # Remove from git
-    git rm $file.FullName
-    # Remove from local directory
+# Find and delete older versions of the files (based on modification date)
+$olderDocxFiles = Get-ChildItem -Filter "*.docx" | Where-Object { $_.FullName -ne $latestDocx.FullName }
+$olderPdfFiles = Get-ChildItem -Filter "*.pdf" | Where-Object { $_.FullName -ne $latestPdf.FullName }
+
+foreach ($file in $olderDocxFiles + $olderPdfFiles) {
+    Write-Host "Deleting old file: $($file.FullName)" -ForegroundColor Yellow
     Remove-Item $file.FullName -Force
+    git rm $file.FullName  # Remove from Git as well
 }
 
 # Rename the new files to "Cory Starks Resume"
 Rename-Item $latestDocx.FullName $newDocxFile -Force
 Rename-Item $latestPdf.FullName $newPdfFile -Force
 
-# Add the new files to Git
+# Add the new files and the script itself to Git
 git add $newDocxFile
 git add $newPdfFile
 git add $scriptFile  # Add the script itself to the repository
